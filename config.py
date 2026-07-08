4b41ba5ed5643b8bbe9e9942e59e5fb950ccfbf2
gg8 <max8086243@gmail.com>
2026-07-08 21:52:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,1 @@
-# config.py
-BOT_TOKEN = "8432885567:AAHQcQFSOUZZZvdDLYWK2TlWSOTRLHKukMc"  # СЮДА ТОКЕН БОТА ОТ @BotFather
-ADMIN_ID = 8548452512  # СЮДА ТВОЙ TELEGRAM ID (узнай у @userinfobot)
+Ну все ди отсюда 
